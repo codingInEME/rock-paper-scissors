b0561a3ca6f7b6b2031da897f69232286f30e5f4
codingInEME <107263899+codingInEME@users.noreply.github.com>
2023-08-14 18:22:26 +0500

    Add playRound

--- index.js
+++ index.js
@@ -5,3 +5,30 @@ function getComputerChoice() {
 
     return idToName[choice];
 }
+
+function playRound(playerSelection, computerSelection) {
+    let outcome;
+    let beat;
+
+    playerSelection = capitalize(playerSelection); // only capitalize the first letter
+
+    if (playerSelection === computerSelection)
+        return `It's a Draw! ${playerSelection} cannot beat ${computerSelection}`;
+    else
+        if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+            (playerSelection === "Paper" && computerSelection === "Rock") ||
+            (playerSelection === "Scissors" && computerSelection === "Paper")) {
+            outcome = "Win";
+            beat = `${playerSelection} beats ${computerSelection}`;
+        }
+        else {
+            outcome = "Lose";
+            beat = `${computerSelection} beats ${playerSelection}`;
+        }
+
+    return `You ${outcome} ${beat}`;
+}
+
+function capitalize(str) {
+    return str[0].toUpperCase() + (str.slice(1)).toLowerCase();
+}
