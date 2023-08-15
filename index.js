a02ccc5c952829ff7eb6dd601bc29b9b94b11117
codingInEME <107263899+codingInEME@users.noreply.github.com>
2023-08-15 15:38:44 +0500

    Add game functionality
    - playRound no longer returns string, this logic is moved to game func
    - playRound returns -1 for draw, true for win, false for loss

--- index.js
+++ index.js
@@ -7,28 +7,56 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let outcome;
-    let beat;
-
     playerSelection = capitalize(playerSelection); // only capitalize the first letter
 
     if (playerSelection === computerSelection)
-        return `It's a Draw! ${playerSelection} cannot beat ${computerSelection}`;
+        return -1; // indicates draw
     else
-        if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+        return (playerSelection === "Rock" && computerSelection === "Scissors") ||
             (playerSelection === "Paper" && computerSelection === "Rock") ||
-            (playerSelection === "Scissors" && computerSelection === "Paper")) {
+            (playerSelection === "Scissors" && computerSelection === "Paper");
+}
+
+function game(rounds) {
+    let playerWins = 0;
+    let computerWins = 0;
+
+    for (let i = 0; i < rounds; i++) {
+        let outcome;
+        let beat;
+
+        let playerSelection = capitalize(prompt("Enter your choice: (Rock, Paper, Scissors)"));
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result === -1) {
+            console.log(`It's a Draw! ${playerSelection} cannot beat ${computerSelection}`);
+            continue;
+        }
+        else if (result === true) {
             outcome = "Win";
             beat = `${playerSelection} beats ${computerSelection}`;
+            playerWins++;
         }
         else {
             outcome = "Lose";
             beat = `${computerSelection} beats ${playerSelection}`;
+            computerWins++;
         }
 
-    return `You ${outcome} ${beat}`;
+        console.log(`You ${outcome}! ${beat}`);
+    }
+
+    if (playerWins > computerWins)
+        console.log("Congratulation! You won the game!");
+    else if (playerWins < computerWins)
+        console.log("You lost the game! Better luck next time!");
+    else
+        console.log("The game is a draw!");
 }
 
 function capitalize(str) {
     return str[0].toUpperCase() + (str.slice(1)).toLowerCase();
 }
+
+game(5);
